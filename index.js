5bedcb762177eef4f252568f7018062ee93377cb
evertejada <evertejada3@gmail.com>
2025-06-28 13:16:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Bienvenido a Cuentos Eternos</h1>
-      <p>Crea cuentos personalizados cada noche para tus hijos.</p>
-    </div>
-  );
-}
+

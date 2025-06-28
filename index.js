4764e9418295f3c192b1dbeb7461bf1af295a318
evertejada <evertejada3@gmail.com>
2025-06-28 12:53:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenido a Cuentos Eternos</h1>
+      <p>Crea cuentos personalizados cada noche para tus hijos.</p>
+    </div>
+  );
+}

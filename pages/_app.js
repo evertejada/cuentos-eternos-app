1c24ded754acd15963ad0340fe45879fa8ececc6
evertejada <evertejada3@gmail.com>
2025-07-05 11:52:14 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'; // Asegúrate de que este archivo exista
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
